1d91e43d6dbc65fd7e3d11a495a676db748f514c
Yuya Akiyama <akiyama@zaty.jp>
2024-11-27 18:54:37 +0900

    ビルドできるように修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_PAGES
+    ? '/testButtonComponent/'
+    : './',
   resolve: {
     alias: {
-      '~': '/src'
-    }
+      '~': path.resolve(__dirname, 'src'),
+    },
+    extensions: ['.js', '.ts', '.jsx', '.tsx'],
   },
   css: {
     modules: {
       localsConvention: 'camelCase',
       generateScopedName: '[name]__[local]__[hash:base64:7]',
     },
-  }
+  },
 })
